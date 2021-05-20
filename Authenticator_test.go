f143beaad8d55d7767bee5bd29049c3141c817a8
J0eppp <joep.van.dijk04@gmail.com>
2021-05-20 15:56:29 +0200

    Added tests for Authenticator.CheckPassword, Authenticator.CheckPassword passed the tests

--- Authenticator_test.go
+++ Authenticator_test.go
@@ -5,22 +5,132 @@ import (
 	"time"
 )
 
+// This is all for testing purposes, normally you would store this information in a real database
+type user struct {
+	Username string
+	Hash string
+	Salt string
+}
+
+var userCollection []user
+var sessionCollection []Session
+
+func getUserPasswordAndSalt(username string) (string, string, error) {
+	for _, user := range userCollection {
+		if user.Username == username {
+			return user.Hash, user.Salt, nil
+		}
+	}
+	return "", "", nil
+}
+
+func getSessionFromDatabase(sessionToken string) Session {
+	for _, session := range sessionCollection {
+		if session.SessionToken == sessionToken && session.Expires < time.Now().Unix() {
+			return session
+		}
+	}
+	return Session{}
+}
+
+func saveSessionToDatabase(uid string, session Session) error {
+	sessionCollection = append(sessionCollection, session)
+	return nil
+}
+
 func TestAuthenticator(t *testing.T) {
 	//authenticator := Authenticator{
 	//	SaltSize: 32,
 	//	Iterations: 10000,
 	//	KeyLength: 64,
 	//}
-	authenticator := NewAuthenticator(func(sessionToken string) Session { return Session{} }, func(uid string, session Session) error { return nil }, "/login", 32, 10000, 64, func(username string) (string, string, error) { return "", "", nil})
+	authenticator := NewAuthenticator(getSessionFromDatabase, saveSessionToDatabase, "/login", 32, 10000, 64, getUserPasswordAndSalt)
 	password := "test123"
 	t.Log("Hashing password: " + password)
 
 	start := time.Now()
 
-	hash := authenticator.Hash(password, authenticator.CreateSalt())
+	salt := authenticator.CreateSalt()
+	hash := authenticator.Hash(password, salt)
+	u := user{
+		Username: "test",
+		Hash:     string(hash),
+		Salt:     string(salt),
+	}
+	userCollection = append(userCollection, u)
 
 	elapsed := time.Since(start)
 
-	t.Log(string(hash))
+	t.Log("Hash: " + string(hash))
+	t.Log("Salt: " + string(salt))
 	t.Logf("Hash took %s", elapsed)
+
+	// Test CheckPassword
+	// Give it a correct password
+	ok, err := authenticator.CheckPassword(u.Username, "test123")
+	if err != nil {
+		t.Failed()
+		t.Error(err.Error())
+	}
+	if !ok {
+		t.Failed()
+		t.Error("Password validation failed while it should have succeeded")
+		t.Errorf("User: %+v\n", u)
+	}
+
+	// Give it an incorrect password
+	ok, err = authenticator.CheckPassword(u.Username, "test")
+	if err != nil {
+		t.Failed()
+		t.Error(err.Error())
+	}
+	if ok {
+		t.Failed()
+		t.Error("CheckPassword says the password is correct while it is not")
+		t.Errorf("User: %+v\n", u)
+	}
+
+	ok, err = authenticator.CheckPassword(u.Username, "")
+	if err != nil {
+		t.Failed()
+		t.Error(err.Error())
+	}
+	if ok {
+		t.Failed()
+		t.Error("CheckPassword says the password is correct while it is not")
+		t.Errorf("User: %+v\n", u)
+	}
+
+	ok, err = authenticator.CheckPassword("", "test123")
+	if err != nil {
+		t.Failed()
+		t.Error(err.Error())
+	}
+	if ok {
+		t.Failed()
+		t.Error("CheckPassword says the password is correct while it is not")
+		t.Errorf("User: %+v\n", u)
+	}
+
+	ok, err = authenticator.CheckPassword("", "")
+	if err != nil {
+		t.Failed()
+		t.Error(err.Error())
+	}
+	if ok {
+		t.Failed()
+		t.Error("CheckPassword says the password is correct while it is not")
+		t.Errorf("User: %+v\n", u)
+	}
+
+	ok, err = authenticator.CheckPassword("incorrectusername", "test123")
+	if err != nil {
+		t.Failed()
+		t.Error(err.Error())
+	}
+	if ok {
+		t.Failed()
+		t.Error("CheckPassword says the password is correct while it is not")
+		t.Errorf("User: %+v\n", u)
+	}
 }

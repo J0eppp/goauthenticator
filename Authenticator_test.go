b42b0fc27c15a3ca9c53ae2f72beaec0750755d7
J0eppp <joep.van.dijk04@gmail.com>
2021-05-20 15:27:26 +0200

    Added CheckPassword method, refactored some code and added NewAuthenticator function

--- Authenticator_test.go
+++ Authenticator_test.go
@@ -5,16 +5,22 @@ import (
 	"time"
 )
 
-func Test_test(t *testing.T) {
-	authenticator := Authenticator{}
+func TestAuthenticator(t *testing.T) {
+	//authenticator := Authenticator{
+	//	SaltSize: 32,
+	//	Iterations: 10000,
+	//	KeyLength: 64,
+	//}
+	authenticator := NewAuthenticator(func(sessionToken string) Session { return Session{} }, func(uid string, session Session) error { return nil }, "/login", 32, 10000, 64, func(username string) (string, string, error) { return "", "", nil})
 	password := "test123"
+	t.Log("Hashing password: " + password)
 
 	start := time.Now()
 
-	hash, _ := authenticator.Hash(password, 32, 10000, 64)
+	hash := authenticator.Hash(password, authenticator.CreateSalt())
 
 	elapsed := time.Since(start)
-	t.Logf("Hash took %s", elapsed)
 
 	t.Log(string(hash))
+	t.Logf("Hash took %s", elapsed)
 }
--- goauthenticator.go
+++ goauthenticator.go
@@ -13,10 +13,49 @@ type Config struct {
 
 type Authenticator struct {
 	SessionHandler SessionHandler
+	SaltSize int
+	Iterations int
+	KeyLength int
+	GetUserPasswordAndSalt func(username string) (string, string, error)
 }
 
+func NewAuthenticator(getSessionFromDatabase func(sessionToken string) Session, saveSessionToDatabase func(uid string, session Session) error, redirectURI string, saltSize int, iterations int, keyLength int, getUserPasswordAndSalt func(username string) (string, string, error)) Authenticator {
+	return Authenticator{
+		SessionHandler: SessionHandler{
+			GetSessionFromDatabase: getSessionFromDatabase,
+			SaveSessionToDatabase:  saveSessionToDatabase,
+			Config: Config{
+				RedirectURI: redirectURI,
+			},
+		},
+		SaltSize:               saltSize,
+		Iterations:             iterations,
+		KeyLength:              keyLength,
+		GetUserPasswordAndSalt: getUserPasswordAndSalt,
+	}
+}
 
-func (auth *Authenticator) CreateRandomByteArray(alphabet []byte, size int) []byte {
+// CheckPassword checks if the password is correct with the data from the database
+func (auth *Authenticator) CheckPassword(username string, password string) (bool, error) {
+	// Get the hash and salt from the database
+	dbHash, dbSalt, err := auth.GetUserPasswordAndSalt(username)
+	if err != nil {
+		return false, err
+	}
+
+	// Hash the entered password
+	hash := auth.Hash(password, []byte(dbSalt))
+	if string(hash) != dbHash {
+		// Hashes do not match
+		return false, nil
+	}
+
+	// Hashed match
+	return true, nil
+}
+
+// Create a random []byte
+func (auth *Authenticator) createRandomByteArray(alphabet []byte, size int) []byte {
 	b := make([]byte, size)
 
 	rand.Seed(time.Now().UnixNano())
@@ -28,9 +67,15 @@ func (auth *Authenticator) CreateRandomByteArray(alphabet []byte, size int) []by
 	return b
 }
 
-func (auth *Authenticator) Hash(password string, saltSize int, iterations int, keyLength int) ([]byte, []byte) {
-	salt := auth.CreateRandomByteArray([]byte("abcdefghijklmnopqrstABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(){}[]-=_+"), saltSize)
-	hash := pbkdf2.Key([]byte(password), salt, iterations, keyLength, sha512.New)
+// CreateSalt Create a salt
+func (auth *Authenticator) CreateSalt() []byte {
+	return auth.createRandomByteArray([]byte("abcdefghijklmnopqrstABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(){}[]-=_+"), auth.SaltSize)
+}
 
-	return hash, salt
+// Hash hashes the password and salt using the configuration from the Authenticator object
+func (auth *Authenticator) Hash(password string, salt []byte) []byte {
+	//salt := auth.CreateSalt()
+	hash := pbkdf2.Key([]byte(password), salt, auth.Iterations, auth.KeyLength, sha512.New)
+
+	return hash
 }
